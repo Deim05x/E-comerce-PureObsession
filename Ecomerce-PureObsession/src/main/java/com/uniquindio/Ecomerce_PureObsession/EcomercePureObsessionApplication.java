5f80d9bb224f20fd6744e894cf31ae970aeb98e0
Deim05x <deividm.cananp@uqvirtual.edu.co>
2026-09-08 20:05:15 -0500

    Primer Commit

---
+++ Ecomerce-PureObsession/src/main/java/com/uniquindio/Ecomerce_PureObsession/EcomercePureObsessionApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.Ecomerce_PureObsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomercePureObsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomercePureObsessionApplication.class, args);
+	}
+
+}
---
+++ Ecomerce-PureObsession/src/test/java/com/uniquindio/Ecomerce_PureObsession/EcomercePureObsessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.Ecomerce_PureObsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomercePureObsessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
