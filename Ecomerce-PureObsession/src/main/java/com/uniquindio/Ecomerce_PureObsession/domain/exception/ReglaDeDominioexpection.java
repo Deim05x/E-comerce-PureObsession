ab14b917a2a2c188fc5659fa591a4ce76a8b79fd
Deim05x <deividm.cananp@uqvirtual.edu.co>
2026-09-10 10:42:58 -0500

    creacion de ramas y carpetas

---
+++ Ecomerce-PureObsession/src/main/java/com/uniquindio/Ecomerce_PureObsession/domain/exception/ReglaDeDominioexpection.java
@@ -0,0 +1,7 @@
+package com.uniquindio.Ecomerce_PureObsession.domain.exception;
+
+public class ReglaDeDominioexpection extends RuntimeException {
+    public ReglaDeDominioexpection(String message) {
+        super(message);
+    }
+}
